0e16d3668f1e14b1d4d7d496f0a89398362cefa4
717030885@qq.com <li19872011>
2019-05-29 22:02:14 +0800

    1

---
+++ static/js/local-zhCN.js
@@ -0,0 +1,12 @@
+/* ================================================================================
+ * Item Name: 
+ * Version: 
+ * Author: shlee on 2017/12/21 0021
+ * Author URL: 
+ **============================================================================= */
+var TAB_TITLE_SETTINGS = '设定';
+var TAB_TITLE_SCENE = '场景';
+var TAB_TITLE_LIGHTING = '光照';
+var TAB_TITLE_MATERIAL = '纹理与材质';
+var TAB_TITLE_ANNOTATION = '注释';
+var TAB_TITLE_SOUND = '声音';
